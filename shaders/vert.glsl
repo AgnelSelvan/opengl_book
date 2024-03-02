9f565abc1d78a272957bc22526006e2125ec48f4
AgnelSelvan <agnelselvan007@gmail.com>
2024-03-02 14:58:49 +0530

    Revert "Perspective projection"
    This reverts commit 3dd146ad23248464b903faa8e28d05498fc27334.

--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -6,10 +6,9 @@ layout(location=1) in vec3 vertexColors;
 out vec3 v_vertexColors;
 
 uniform mat4 uModelMatrix; // Uniform Variable
-uniform mat4 uPerspectiveMatrix; // Uniform Variable
 
 void main(){
     v_vertexColors = vertexColors;
-    vec4 newPosition = uPerspectiveMatrix * uModelMatrix * vec4(position, 1.0f);
-    gl_Position = newPosition;
+    vec4 newPosition = uModelMatrix * vec4(position, 1.0f);
+    gl_Position = vec4(newPosition.x, newPosition.y, newPosition.z, 1.0f);
 }
