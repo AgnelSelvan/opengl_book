2c2d5aee9298f2a77de2e635a27dae43c662bdad
AgnelSelvan <agnelselvan007@gmail.com>
2024-06-07 22:31:07 +0530

    Added ImGUI and made Cube rendering dynamic

--- Makefile
+++ Makefile
@@ -1,20 +1,25 @@
 BUILD_DIR=./bin
 APP_NAME=app.out
 CURRENT_DIR=.
+IMGUI_DIR=$(DIR_INCLUDE)/imgui
 
-DIR_INCLUDE=-I$(CURRENT_DIR)/dependencies/includes
+DIR_INCLUDE=$(CURRENT_DIR)/dependencies/includes
 GLFW_LIBRARY=$(CURRENT_DIR)/dependencies/lib/GLFW/libglfw.3.3.dylib
-
 
 ADDITIONAL=-fcolor-diagnostics -fansi-escape-codes -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo -framework CoreFoundation -Wno-deprecated
 
-INCLUDES=$(DIR_INCLUDE)
+INCLUDES=-I$(DIR_INCLUDE)
+IMGUI_INCLUDES=-I$(IMGUI_DIR)
 LIBRARIES=$(GLFW_LIBRARY)
 
 GLAD=glad.c
 
+SOURCES = *.cpp
+SOURCES += $(IMGUI_DIR)/imgui.cpp $(IMGUI_DIR)/imgui_demo.cpp $(IMGUI_DIR)/imgui_draw.cpp $(IMGUI_DIR)/imgui_tables.cpp $(IMGUI_DIR)/imgui_widgets.cpp
+SOURCES += $(IMGUI_DIR)/imgui_impl_glfw.cpp $(IMGUI_DIR)/imgui_impl_opengl3.cpp
+
 build:
-	g++ -std=c++20 *.cpp -I. -g -o $(BUILD_DIR)/$(APP_NAME) $(GLAD) $(INCLUDES) $(LIBRARIES) $(ADDITIONAL)
+	g++ -std=c++20 $(SOURCES) -I. -g -o $(BUILD_DIR)/$(APP_NAME) $(GLAD) $(INCLUDES) $(LIBRARIES) $(ADDITIONAL) $(IMGUI_INCLUDES)
 
 all: build
 	clear
--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #define STB_IMAGE_IMPLEMENTATION
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include "stb_image.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -6,6 +9,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
+#include <list>
 #include "shader_h.h"
 #include "camera.h"
 
@@ -15,9 +19,8 @@ void mouseCallback(GLFWwindow* window, double xPos, double yPos);
 void scrollCallback(GLFWwindow* window, double xOffset, double yOffset);
 
 
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
+unsigned int SCR_WIDTH = 800;
+unsigned int SCR_HEIGHT = 600;
 
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 bool firstMouse = true;
@@ -29,8 +32,53 @@ float lastX =  SCR_WIDTH / 2.0;
 float lastY =  SCR_HEIGHT / 2.0;
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
+struct Material{
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+    float shininess;
+};
+
+struct Light{
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+    glm::vec3 position;
+};
+
+struct Cube{
+    Material material;
+    Light light;
+    glm::vec3 position;
+    uint32_t id;
+};
+
+Cube getEmptyCubeObject(){
+    Material material;
+    material.ambient = glm::vec3(0.0215f, 0.1745f, 0.0215f);
+    material.diffuse = glm::vec3(0.07568, 0.61424, 0.07568);
+    material.specular = glm::vec3(0.633, 0.727811, 0.633);
+    material.shininess = 0.6f;
+
+    Light light;
+    light.ambient = glm::vec3(0.2f, 0.2f, 0.2f);
+    light.diffuse = glm::vec3(0.5f, 0.5f, 0.5f);
+    light.specular = glm::vec3(1.0f, 1.0f, 1.0f);
+
+    Cube cube;
+    cube.material = material;
+    cube.light = light;
+    cube.position = glm::vec3(0.0f, 1.5f, 0.f);
+    cube.id = rand() % 100;
+
+    return cube;
+}
+bool mouseCanDrag = true;
+
 int main()
 {
+    std::list<Cube> cubeList;
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -39,6 +87,11 @@ int main()
     #ifdef __APPLE__
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     #endif
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+    SCR_WIDTH = mode->width;
+    SCR_HEIGHT = mode->height;
 
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Playground", NULL, NULL);
     if (window == NULL)
@@ -130,9 +183,16 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    // glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glfwSetCursorPosCallback(window, mouseCallback);
     glfwSetScrollCallback(window, scrollCallback);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
 
     while (!glfwWindowShouldClose(window))
     {
@@ -145,6 +205,15 @@ int main()
         glClearColor(0.f, 0.f, 0.f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        if(ImGui::IsWindowHovered(ImGuiHoveredFlags_AnyWindow | ImGuiHoveredFlags_AllowWhenBlockedByActiveItem)){
+            mouseCanDrag = false;
+        }else{
+            mouseCanDrag = true;
+        }
+
         lightPos.z = 1.f + sin(glfwGetTime()) * 2.f;
         lightPos.y = cos(glfwGetTime())* 2.f ;
 
@@ -154,14 +223,8 @@ int main()
         lightingShader.setVec3("viewPos", camera.position.x, camera.position.y, camera.position.z);
         lightingShader.setVec3("light.position", lightPos.x, lightPos.y, lightPos.z);
 
-        // Cube 1
-        lightingShader.setVec3("material.ambient", 0.0215f, 0.1745f, 0.0215f);
-        lightingShader.setVec3("material.diffuse", 0.07568, 0.61424, 0.07568);
-        lightingShader.setVec3("material.specular", 0.633, 0.727811, 0.633);
-        lightingShader.setFloat("material.shininess", 0.6f);
-        lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-        lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
-        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+        // world transformation
+        glm::mat4 model = glm::mat4(1.0f);
 
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -169,52 +232,25 @@ int main()
         lightingShader.setMat4("projection", projection);
         lightingShader.setMat4("view", view);
 
-        // world transformation
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, 1.5f, 0.f));
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(90.0f), glm::vec3(1.0f, 1.0f, 0.0f));
-        lightingShader.setMat4("model", model);
+        for (auto &&cube : cubeList)
+        {
+            lightingShader.setVec3("material.ambient", cube.material.ambient.x, cube.material.ambient.y, cube.material.ambient.z);
+            lightingShader.setVec3("material.diffuse", cube.material.diffuse.x, cube.material.diffuse.y, cube.material.diffuse.z);
+            lightingShader.setVec3("material.specular", cube.material.specular.x, cube.material.specular.y, cube.material.specular.z);
+            lightingShader.setFloat("material.shininess", cube.material.shininess);
+            lightingShader.setVec3("light.ambient", cube.light.ambient.x, cube.light.ambient.y, cube.light.ambient.z);
+            lightingShader.setVec3("light.diffuse", cube.light.diffuse.x, cube.light.diffuse.y, cube.light.diffuse.z);
+            lightingShader.setVec3("light.specular", cube.light.specular.x, cube.light.specular.y, cube.light.specular.z);
 
-        // render the 1 cube
-        glBindVertexArray(cubeVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
 
-        // Cube 2
-        lightingShader.setVec3("material.ambient", 0.19225,	0.19225,	0.19225);
-        lightingShader.setVec3("material.diffuse", 0.50754,	0.50754,	0.50754);
-        lightingShader.setVec3("material.specular", 0.508273,	0.508273,	0.508273);
-        lightingShader.setFloat("material.shininess", 0.4);
-        lightingShader.setVec3("light.ambient", 0.8f, 0.8f, 0.8f);
-        lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
-        lightingShader.setVec3("light.specular", 0.7f, 0.7f, 0.7f);
+            model = glm::translate(model, glm::vec3(cube.position.x, cube.position.y, cube.position.z));
+            model = glm::rotate(model, (float)glfwGetTime() * glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+            lightingShader.setMat4("model", model);
 
-        // world transformation
-        model = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(1.0f, 1.0f, 1.0f));
-        lightingShader.setMat4("model", model);
-
-        // render the 2 cube
-        glBindVertexArray(cubeVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-
-        // Cube 3
-        lightingShader.setVec3("material.ambient",0.0,	0.05,	0.05);
-        lightingShader.setVec3("material.diffuse", 0.4,	0.5,	0.5);
-        lightingShader.setVec3("material.specular", 0.04,	0.7,	0.7);
-        lightingShader.setFloat("material.shininess", 0.078125);
-        lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-        lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
-        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
-
-        // world transformation
-        model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, -1.5f, 0.f));
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(40.0f), glm::vec3(1.0f, 1.0f, 0.0f));
-        lightingShader.setMat4("model", model);
-
-        // render the 3 cube
-        glBindVertexArray(cubeVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+            // render the 1 cube
+            glBindVertexArray(cubeVAO);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         ourShader.use();
         ourShader.setMat4("projection", projection);
@@ -227,9 +263,144 @@ int main()
         glBindVertexArray(lightCubeVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
+        ImGui::Begin("Change Settings");
+        ImGui::Text("Add or Remove Cube");
+        if(ImGui::Button("-")){
+            cubeList.pop_front();
+        }
+        ImGui::SameLine();
+        ImGui::Text("%zu", cubeList.size()),
+        ImGui::SameLine();
+        if(ImGui::Button("+")){
+            cubeList.push_back(getEmptyCubeObject());
+        }
+
+        for (auto &&cube : cubeList)
+        {
+            if(ImGui::CollapsingHeader("Cube")){
+                ImGui::Text("Cube Position");
+                std::string text1 = "##1" + std::to_string(cube.id);
+                std::string text2 = "##2" + std::to_string(cube.id);
+                std::string text3 = "##3" + std::to_string(cube.id);
+                ImGui::SliderFloat(text1.c_str(), &cube.position.x, -100.0f, 100.f);
+                ImGui::SliderFloat(text2.c_str(), &cube.position.y, -100.0f, 100.f);
+                ImGui::SliderFloat(text3.c_str(), &cube.position.z, -100.0f, 100.f);
+
+                if(ImGui::CollapsingHeader("Material")){
+
+                    ImGui::Text("Ambient");
+                    text1 = "##4" + std::to_string(cube.id);
+                    text2 = "##5" + std::to_string(cube.id);
+                    text3 = "##6" + std::to_string(cube.id);
+                    if(ImGui::BeginTable("Ambient", 3)){
+                        ImGui::TableNextRow();
+                        ImGui::TableSetColumnIndex(0);
+                        ImGui::SliderFloat(text1.c_str(), &cube.material.ambient.x, 0.f, 1.f);
+                        ImGui::TableSetColumnIndex(1);
+                        ImGui::SliderFloat(text2.c_str(), &cube.material.ambient.y, 0.f, 1.f);
+                        ImGui::TableSetColumnIndex(2);
+                        ImGui::SliderFloat(text3.c_str(), &cube.material.ambient.z, 0.f, 1.f);
+                        ImGui::EndTable();
+                    }
+
+                    ImGui::Text("Diffuse");
+                    text1 = "##7" + std::to_string(cube.id);
+                    text2 = "##8" + std::to_string(cube.id);
+                    text3 = "##9" + std::to_string(cube.id);
+                    if(ImGui::BeginTable("Diffuse", 3)){
+                        ImGui::TableNextRow();
+                        ImGui::TableSetColumnIndex(0);
+                        ImGui::SliderFloat(text1.c_str(), &cube.material.diffuse.x, 0.f, 1.f);
+                        ImGui::TableSetColumnIndex(1);
+                        ImGui::SliderFloat(text2.c_str(), &cube.material.diffuse.y, 0.f, 1.f);
+                        ImGui::TableSetColumnIndex(2);
+                        ImGui::SliderFloat(text3.c_str(), &cube.material.diffuse.z, 0.f, 1.f);
+                        ImGui::EndTable();
+                    }
+
+                    ImGui::Text("Specular");
+                    text1 = "##10" + std::to_string(cube.id);
+                    text2 = "##11" + std::to_string(cube.id);
+                    text3 = "##12" + std::to_string(cube.id);
+                    if(ImGui::BeginTable("Specular", 3)){
+                        ImGui::TableNextRow();
+                        ImGui::TableSetColumnIndex(0);
+                        ImGui::SliderFloat(text1.c_str(), &cube.material.specular.x, 0.f, 1.f);
+                        ImGui::TableSetColumnIndex(1);
+                        ImGui::SliderFloat(text2.c_str(), &cube.material.specular.y, 0.f, 1.f);
+                        ImGui::TableSetColumnIndex(2);
+                        ImGui::SliderFloat(text3.c_str(), &cube.material.specular.z, 0.f, 1.f);
+                        ImGui::EndTable();
+                    }
+
+                    ImGui::Text("Shininess");
+                    text3 = "##13" + std::to_string(cube.id);
+                    ImGui::SliderFloat(text3.c_str(), &cube.material.shininess, 0.f, 1.f);
+                }
+
+                if(ImGui::CollapsingHeader("Light")){
+                    ImGui::Text("Ambient");
+                    text1 = "##14" + std::to_string(cube.id);
+                    text2 = "##15" + std::to_string(cube.id);
+                    text3 = "##16" + std::to_string(cube.id);
+                    if(ImGui::BeginTable("Ambient", 3)){
+                        ImGui::TableNextRow();
+                        ImGui::TableSetColumnIndex(0);
+                        ImGui::SliderFloat(text1.c_str(), &cube.light.ambient.x, 0.f, 1.f);
+                        ImGui::TableSetColumnIndex(1);
+                        ImGui::SliderFloat(text2.c_str(), &cube.light.ambient.y, 0.f, 1.f);
+                        ImGui::TableSetColumnIndex(2);
+                        ImGui::SliderFloat(text3.c_str(), &cube.light.ambient.z, 0.f, 1.f);
+                        ImGui::EndTable();
+                    }
+
+                    ImGui::Text("Diffuse");
+                    text1 = "##17" + std::to_string(cube.id);
+                    text2 = "##18" + std::to_string(cube.id);
+                    text3 = "##19" + std::to_string(cube.id);
+                    if(ImGui::BeginTable("Diffuse", 3)){
+                        ImGui::TableNextRow();
+                        ImGui::TableSetColumnIndex(0);
+                        ImGui::SliderFloat(text1.c_str(), &cube.light.diffuse.x, 0.f, 1.f);
+                        ImGui::TableSetColumnIndex(1);
+                        ImGui::SliderFloat(text2.c_str(), &cube.light.diffuse.y, 0.f, 1.f);
+                        ImGui::TableSetColumnIndex(2);
+                        ImGui::SliderFloat(text3.c_str(), &cube.light.diffuse.z, 0.f, 1.f);
+                        ImGui::EndTable();
+                    }
+
+                    ImGui::Text("Specular");
+                    text1 = "##20" + std::to_string(cube.id);
+                    text2 = "##21" + std::to_string(cube.id);
+                    text3 = "##22" + std::to_string(cube.id);
+                    if(ImGui::BeginTable("Diffuse", 3)){
+                        ImGui::TableNextRow();
+                        ImGui::TableSetColumnIndex(0);
+                        ImGui::SliderFloat(text1.c_str(), &cube.light.specular.x, 0.f, 1.f);
+                        ImGui::TableSetColumnIndex(1);
+                        ImGui::SliderFloat(text2.c_str(), &cube.light.specular.y, 0.f, 1.f);
+                        ImGui::TableSetColumnIndex(2);
+                        ImGui::SliderFloat(text3.c_str(), &cube.light.specular.z, 0.f, 1.f);
+                        ImGui::EndTable();
+                    }
+
+                }
+            }
+        }
+
+
+        ImGui::End();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    ImGui_ImplGlfw_Shutdown();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui::DestroyContext();
 
     glDeleteVertexArrays(1, &cubeVAO);
     glDeleteVertexArrays(1, &lightCubeVAO);
@@ -258,6 +429,14 @@ void processInput(GLFWwindow *window)
 }
 
 void mouseCallback(GLFWwindow* window, double xPosIn, double yPosIn){
+    if(!mouseCanDrag){
+        return;
+    }
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE)
+    {
+        firstMouse = true;
+        return;
+    }
     float xPos = static_cast<float>(xPosIn);
     float yPos = static_cast<float>(yPosIn);
 
