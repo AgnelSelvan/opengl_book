3bc057ffb5000bb58a35411f1b1afb5a0534b28f
AgnelTFO <agnelselvan007@gmail.com>
2024-07-02 20:44:14 +0530

    Added Directional Light, Spotlight Light and Point light in a scene

--- readMe.md
+++ readMe.md
@@ -1,3 +1,3 @@
-# Spotlight
+# Added Directional Light, Spotlight Light and Point light in a scene
 
 <img src="./outputs/1.gif">
--- shader_h.h
+++ shader_h.h
@@ -59,7 +59,7 @@ class Shader{
 
         int compileShader(GLenum type, const char* shaderCode, std::string shaderType){
             unsigned int shader;
-            
+
             shader = glCreateShader(type);
             glShaderSource(shader, 1, &shaderCode, NULL);
             glCompileShader(shader);
@@ -73,25 +73,25 @@ class Shader{
         }
 
        void setBool(const std::string &name, bool value) const
-        {         
-            glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
+        {
+            glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
         }
         void setInt(const std::string &name, int value) const
-        { 
-            glUniform1i(glGetUniformLocation(ID, name.c_str()), value); 
+        {
+            glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
         }
         void setFloat(const std::string &name, float value) const
-        { 
-            glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
-        } 
+        {
+            glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+        }
         void setVec4(const std::string &name, float x, float y, float z, float a) const
-        { 
+        {
             glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, a);
-        } 
+        }
         void setVec3(const std::string &name, float x, float y, float z) const
-        { 
+        {
             glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
-        } 
+        }
         void setMat4(const std::string &name, const glm::mat4 &mat) const
         {
             glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
